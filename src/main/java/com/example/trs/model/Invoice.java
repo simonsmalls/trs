da5ea47a3a6618d77b32f4dc873562c66cd192a5
Simon Hazevoets <simonsmalls568@gmail.com>
2022-12-22 13:49:19 +0100

    add constructor, getters & setters in Invoice class

--- src/main/java/com/example/trs/model/Invoice.java
+++ src/main/java/com/example/trs/model/Invoice.java
@@ -8,4 +8,46 @@ public class Invoice {
     private double totalPrice;
     private Project project;
     private boolean closed;
+
+    public Invoice() {
+    }
+
+    public Invoice(LocalDate date, double totalPrice, Project project, boolean closed) {
+        this.date = date;
+        this.totalPrice = totalPrice;
+        this.project = project;
+        this.closed = closed;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
 }
