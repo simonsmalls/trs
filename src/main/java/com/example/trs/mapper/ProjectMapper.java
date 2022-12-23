b3bfd9e26ae077227df6d62a7e428ade6e586317
Simon Hazevoets <simonsmalls568@gmail.com>
2022-12-23 11:22:32 +0100

    adds entities to some non-person classes - jpa unit test succes!

---
+++ src/main/java/com/example/trs/mapper/ProjectMapper.java
@@ -0,0 +1,21 @@
+package com.example.trs.mapper;
+
+import com.example.trs.dto.ProjectDTO;
+import com.example.trs.model.Project;
+
+public class ProjectMapper {
+
+
+    public static Project toProject(ProjectDTO dto
+    ){
+        Project project=new Project();
+
+
+
+
+
+        return project;
+
+
+    }
+}
