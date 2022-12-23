b28220b4fdc914a73b48d44f0452acfbb87c021e
Jens Verheyden <jensverheyden4@hotmail.com>
2022-12-23 11:18:29 +0100

    sql script + time to int

--- src/main/resources/Script.sql
+++ src/main/resources/Script.sql
@@ -100,7 +100,7 @@ create table workingTimes
     workingDate date,
     startTime time,
     endTime time,
-    timeWorked time,
+    timeWorked int,
     employee_id int,
     constraint FK_EMPLOYEE FOREIGN KEY (employee_id) references employees
 );
@@ -133,7 +133,7 @@ create table activities
     category_id int,
     startTime timestamp,
     endTime timestamp,
-    timeSpent time,
+    timeSpent int,
     constraint FK_PERSONS foreign key (employee_id) references employees,
     constraint FK_PROJECTS foreign key (project_id) references projects,
     constraint FK_CATEGORIES foreign key (category_id) references categories
@@ -168,10 +168,10 @@ insert into companies values (2, 'SMALS');
 insert into projects values (1, 2, 'projectnaam', 'projectomschrijving', 2000.00,'2022-08-31', '2022-12-20');
 insert into projects values (2, 1, 'abisproject', 'abisprojectomschrijving', 1000.00,'2022-09-14', '2023-01-22');
 
-insert into workingtimes values (1, '2022-12-21', '09:00:00', '14:30:00', '04:30:00', 9);
-insert into workingtimes values (2, '2022-12-22', '08:00:00', '17:00:00', '08:00:00', 7);
-insert into workingtimes values (3, '2022-12-22', '08:00:00', '17:00:00', '08:00:00', 5);
-insert into workingtimes values (4, '2022-12-22', '08:00:00', '17:00:00', '08:00:00', 4);
+insert into workingtimes values (1, '2022-12-21', '09:00:00', '14:30:00', 280, 9);
+insert into workingtimes values (2, '2022-12-22', '08:00:00', '17:00:00', 480, 10);
+insert into workingtimes values (3, '2022-12-22', '08:00:00', '17:00:00', 480, 3);
+insert into workingtimes values (4, '2022-12-22', '08:00:00', '17:00:00', 480, 4);
 
 insert into categories values (1, 'Teaching');
 insert into categories values (2, 'Administration');
@@ -183,10 +183,11 @@ insert into categories values (6, 'Food prep');
 insert into invoices values (1, '2021-01-28', 20000.00, 1, true);
 insert into invoices values (2, null, null, 2, false);
 
-insert into activities values (1,'uh', 3, 2, 4, '2022-12-22 14:00:00', '2022-12-22  15:00:00', '01:00:00');
-insert into activities values (2, null,  4, 2, 6, '2022-12-22 11:00:00', '2022-12-22  12:30:00', '01:30:00');
-insert into activities values (3, 'ja', 5, 1, 3, '2022-08-11 13:15:00', '2022-08-11 16:00:00', '02:45:00');
-insert into activities values (4, null, 6, 1, 5, '2022-08-10 09:00:00', '2022-08-10  11:45:00', '02:45:00');
+insert into activities (description, employee_id,  project_id, category_id, startTime, endTime, timeSpent) values ('uh', 3, 2, 4, '2022-12-22 14:00:00', '2022-12-22  15:00:00', '60');
+insert into activities (description, employee_id, project_id, category_id, startTime, endTime, timeSpent) values (null,  4, 2, 6, '2022-12-22 11:00:00', '2022-12-22  12:30:00', '90');
+insert into activities ( description, employee_id, project_id, category_id, startTime, endTime, timeSpent) values ('ja', 5, 1, 3, '2022-08-11 13:15:00', '2022-08-11 16:00:00', '165');
+insert into activities (description, employee_id, project_id, category_id, startTime, endTime, timeSpent) values (null, 6, 1, 5, '2022-08-10 09:00:00', '2022-08-10  11:45:00', '165');
+
 
 insert into personRoles values (1, 'Teacher');
 insert into personRoles values (8, 'Manager');
