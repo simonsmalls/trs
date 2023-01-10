f3d36fdbb68e12efe28ab84d22dda031cf7c0f7d
[Kim Wauters] <[solidarity.forever2@gmail.com]>
2023-01-10 11:38:31 +0100

    completely fleshed out Category-ServiceTest

--- src/test/java/com/example/trs/service/AbisCategoryServiceTest.java
+++ src/test/java/com/example/trs/service/AbisCategoryServiceTest.java
@@ -18,13 +18,44 @@ class AbisCategoryServiceTest {
     CategoryService categoryService;
 
     @Test@Transactional
-    void findCategoryByIDTest() throws CategoryNotFoundException {
+    void findCategoryByIdTest() throws CategoryNotFoundException {
         assertEquals("Les geven", categoryService.findCategoryByID(1).getName());
         assertEquals("Administratie", categoryService.findCategoryByID(2).getName());
         assertEquals("Accounting", categoryService.findCategoryByID(3).getName());
         assertEquals("Sales", categoryService.findCategoryByID(4).getName());
         assertEquals("Studeren", categoryService.findCategoryByID(5).getName());
         assertEquals("Voorbereiding eten", categoryService.findCategoryByID(6).getName());
+    }
+
+    @Test@Transactional
+    void findCategoryByNameTest() throws CategoryNotFoundException {
+        assertEquals(1, categoryService.findCategoryByName("Les geven").getId());
+        assertEquals(2, categoryService.findCategoryByName("Administratie").getId());
+        assertEquals(3, categoryService.findCategoryByName("Accounting").getId());
+        assertEquals(4, categoryService.findCategoryByName("Sales").getId());
+        assertEquals(5, categoryService.findCategoryByName("Studeren").getId());
+        assertEquals(6, categoryService.findCategoryByName("Voorbereiding eten").getId());
+    }
+
+    @Test@Transactional
+    void findCategoryByIdThrowsNotFoundExceptionTest() {
+        assertThrows(CategoryNotFoundException.class, ()-> categoryService.findCategoryByID(-5));
+    }
+
+    @Test@Transactional
+    void findCategoryByNameThrowsNotFoundExceptionTest() {
+        assertThrows(CategoryNotFoundException.class, ()-> categoryService.findCategoryByName("Hocus Pocus Pas"));
+    }
+
+    @Test@Transactional
+    void getAllCategoriesTest() {
+        assertEquals("Les geven", categoryService.getAll().get(0).getName());
+        assertEquals("Administratie", categoryService.getAll().get(1).getName());
+        assertEquals("Accounting", categoryService.getAll().get(2).getName());
+        assertEquals("Sales", categoryService.getAll().get(3).getName());
+        assertEquals("Studeren", categoryService.getAll().get(4).getName());
+        assertEquals("Voorbereiding eten", categoryService.getAll().get(5).getName());
+        assertEquals(6, categoryService.getAll().size());
     }
 
 }
